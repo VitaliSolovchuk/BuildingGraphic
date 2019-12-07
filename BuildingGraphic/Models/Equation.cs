7d6ef91dec0ea7734940ae7ca5ca0c8cf792b66b
VitaliSolovchuk <vitalisolovchuk@gmail.com>
2019-12-07 20:39:35 +0300

    added DTO models and validation seting

--- BuildingGraphic/Models/Equation.cs
+++ BuildingGraphic/Models/Equation.cs
@@ -1,11 +1,19 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BuildingGraphic.Models
 {
     public class Equation
     {
+        [Required]
+        [Display(Name = "Coefficient x^2")]
         public float CoefficientZeroDegrees { get; set; }
+        [Required]
+        [Display(Name = "Coefficient x^1")]
         public float CoefficientFirstDegrees { get; set; }
+        [Required]
+        [Display(Name = "Coefficient x^0")] 
         public float CoefficientSecondDegrees { get; set; }
     }
 }
--- BuildingGraphic/Models/EquationDTO.cs
+++ BuildingGraphic/Models/EquationDTO.cs
@@ -1,11 +1,25 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BuildingGraphic.Models
 {
     public class EquationDTO : Equation
     {
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "from")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некорректныая точка")]
         public float StartPosition { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "to")]
+        [StringLength(3, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некорректныая точка")]
         public float StopPosition { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "step")]
+        [StringLength(4, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некорректный шаг")]
         public float Step { get; set; }
 
     }
