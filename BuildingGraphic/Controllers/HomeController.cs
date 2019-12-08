65872e3c8046fa2bf8502a397aec0dd647b75534
VitaliSolovchuk <vitalisolovchuk@gmail.com>
2019-12-08 22:31:10 +0300

    was trying creting table

--- BuildingGraphic/Controllers/HomeController.cs
+++ BuildingGraphic/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BuildingGraphic.Models;
 using System.Web.Mvc;
 
 namespace BuildingGraphic.Controllers
@@ -11,6 +8,13 @@ namespace BuildingGraphic.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index (EquationDTO equationDTO)
+        {
+            string stringa = "text mesage";
+            return PartialView(stringa);
         }
 
         public ActionResult About()
--- BuildingGraphic/Models/EquationDTO.cs
+++ BuildingGraphic/Models/EquationDTO.cs
@@ -6,18 +6,20 @@ namespace BuildingGraphic.Models
 {
     public class EquationDTO : Equation
     {
-        [Required(ErrorMessage = "Поле должно быть установлено")]
         [Display(Name = "from")]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [StringLength(3, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
         [RegularExpression(@"[0-9]", ErrorMessage = "Некорректныая точка")]
         public float StartPosition { get; set; }
-        [Required(ErrorMessage = "Поле должно быть установлено")]
+
         [Display(Name = "to")]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [StringLength(3, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
         [RegularExpression(@"[0-9]", ErrorMessage = "Некорректныая точка")]
+        
         public float StopPosition { get; set; }
-        [Required(ErrorMessage = "Поле должно быть установлено")]
         [Display(Name = "step")]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [StringLength(4, MinimumLength = 1, ErrorMessage = "Братан, я не супе ПК. Уменьши запросы=)")]
         [RegularExpression(@"[0-9]", ErrorMessage = "Некорректный шаг")]
         public float Step { get; set; }
