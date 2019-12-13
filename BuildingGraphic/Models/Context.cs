7e47ad89ee5309bda85ac672b00da137bcf4343c
VitaliSolovchuk <vitalisolovchuk@gmail.com>
2019-12-13 05:23:23 +0300

    added database

---
+++ BuildingGraphic/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+
+namespace BuildingGraphic.Models
+{
+    public class Context : DbContext
+    {
+        // Лично я тут вижу исполльзование ДБ как сохранение ранее введенных
+        //уравнений, чтобы позже можно было вернуться. Шаг будет только засорять
+        public DbSet<Equation> Equations { get; set; }
+        public DbSet<Point> Points { get; set; }
+    }
+}
---
+++ BuildingGraphic/Models/ContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace BuildingGraphic.Models
+{
+    public class ContextInitializer : DropCreateDatabaseAlways<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            context.Equations.Add(new Equation
+            {
+                CoefficientSecondDegrees = 2,
+                CoefficientFirstDegrees = 3,
+                CoefficientZeroDegrees = 0
+            });
+            base.Seed(context);
+        }
+    }
+}
