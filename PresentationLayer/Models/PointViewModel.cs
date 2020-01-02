1a72c57ee0ac9102662b8988d9cde23bc31c0ab4
VitaliSolovchuk <vitalisolovchuk@gmail.com>
2020-01-03 02:21:39 +0300

    added PresentationLayer

---
+++ PresentationLayer/Models/PointViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+namespace PresentationLayer.Models
+{
+    public class PointViewModel
+    {
+        public int PointId { get; set; }
+        public int ChartId { get; set; }
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+    }
+}
---
+++ PresentationLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("PresentationLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PresentationLayer")]
+[assembly: AssemblyCopyright("© , 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("13150fd6-243e-4a4f-9baf-f40d1ceabb66")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
